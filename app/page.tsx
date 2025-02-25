5d334b6c77b3a59685f034291bc6d80ee9d70ce0
Sena Kobayashi <senaly.0304@gmail.com>
2025-02-25 22:26:05 +0900

    impl three-dimensional-book

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,23 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className='min-h-dvh grid place-content-center bg-white'>
+      <div className='[perspective:1000px] group'>
+        <div className='group w-80 transision duration-1000 group-hover:[transform:translateX(15%)_rotateX(10deg)] aspect-[3/4] [transform-style:preserve-3d] [transform:rotateX(40deg)] relative'>
+          <div className='text-center pt-20 [transform-style:preserve-3d] group-hover:[transform:rotateY(-160deg)] origin-left transision duration-1000 absolute inset-0 bg-zinc-800 text-white p-6 font-bold text-3xl'>
+            <div className='[transform:translateZ(1px)] bg-zinc-800 absolute inset-0' />
+            <h1 className='[transform:translateZ(1px)]'>Sena Kobayashi</h1>
+          </div>
+          <div className='absolute w-5 bg-zinc-800 inset-y-0 left-0 origin-left [transform:rotateY(90deg)]' />
+          <div className='bg-white p-6 border absolute [transform-style:preserve-3d] inset-y-4 left-0 right-4 [transform:translateZ(-1px)] '>
+            <p className='text-black mb-3 font-bold'>Sena Kobayashi</p>
+            <p className='text-black mb-1'>▪ 関西大学 システム理工学部 物理・応用物理学科 物性理論研究室/本多研卒業 (2024)</p>
+            <p className='text-black mb-1'>▪ 卒業論文: 強磁性層多層膜におけるスピンホール効果を介したスピン注入</p>
+            <p className='text-black'>▪ 執筆論文: <a className='underline underline-offset-2' href='https://kansai-u.repo.nii.ac.jp/records/2001438'>Spin Current for Spin-Orbit Torque in Magnetoresistance Structure</a></p>
+            <div className='w-full h-5 [tramsform:rotateX(90deg)] origin-bottom bg-white border absolute bottom-0 inset-x-0' />
+          </div>
+          <div className='absolute [transform:translateZ(-20px)] inset-0 bg-zinc-800 text-white p-6 font-bold text-3xl' />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
